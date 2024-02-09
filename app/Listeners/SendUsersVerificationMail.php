c2df37c3e0c8ef180b5c81178e00725b01192717
Bruno <eletronica.bruno@hotmail.com>
2024-02-09 20:13:16 -0300

    Criar event e listeners para novos usuarios

---
+++ app/Listeners/SendUsersVerificationMail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class SendUsersVerificationMail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        //
+        // Enviando email
+        Log::info('Enviando email de verificação para o usuário...');
+    }
+}
